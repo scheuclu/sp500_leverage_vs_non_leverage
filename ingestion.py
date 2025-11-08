75b936834c08b9e97cb328cf7c04fb933a8984dd
Lukas Scheucher <scheuclu@gmail.com>
2025-11-08 19:55:53 +0000

    Added fly configuration.

---
+++ ingestion.py
@@ -0,0 +1,107 @@
+import os
+
+import requests
+from models import TradableInstrument, Exchange, WorkingSchedule, Position, TimeEvent, Type3
+from dotenv import load_dotenv
+from supabase import Client, create_client
+load_dotenv()
+import time
+
+TRADING212_KEY = os.environ["TRADING212_KEY"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+
+
+
+
+import datetime
+
+# class TzInfo(datetime.tzinfo):
+#     def __init__(self, offset_hours: int):
+#         self.offset = datetime.timedelta(hours=offset_hours)
+#     def utcoffset(self, dt):
+#         return self.offset
+#     def tzname(self, dt):
+#         return f"TzInfo({int(self.offset.total_seconds() / 3600)})"
+#     def dst(self, dt):
+#         return datetime.timedelta(0)
+
+
+
+
+
+
+
+
+url = "https://demo.trading212.com/api/v0/equity/metadata/instruments"
+headers = {
+    "Authorization": TRADING212_KEY,
+    "Content-Type": "application/json",
+}
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+instruments = {d["ticker"]: TradableInstrument(**d) for d in response.json()}
+
+# instruments["VUAGl_EQ"].workingScheduleId
+# instruments["5SPYl_EQ"].workingScheduleId
+# # AAPL_US_EQ
+
+
+# timeEvents = workingSchedules[70].timeEvents
+
+
+def is_exchange_open(timeEvents: list[TimeEvent]):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    last_event=[t for t in timeEvents if t.date<now ][-1]
+    return last_event.type==Type3.OPEN
+
+
+
+url = "https://demo.trading212.com/api/v0/equity/metadata/exchanges"
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+exchanges = [Exchange(**d) for d in response.json()]
+
+workingSchedules: dict[int, WorkingSchedule] = {}
+for exchange in exchanges:
+    for w in exchange.workingSchedules:
+        id = w.id
+        workingSchedules[id] = w
+
+
+INTERVAL = 30  # seconds
+
+next_run = time.time()
+
+while True:
+    start = time.time()
+
+    url = "https://demo.trading212.com/api/v0/equity/portfolio"
+    response = requests.get(url, headers=headers)
+    positions = [Position(**i) for i in response.json()]
+
+    working_schedule_ids = [instruments[p.ticker].workingScheduleId for p in positions]
+    ws = [workingSchedules[id] for id in working_schedule_ids]
+    all_open = all([is_exchange_open(w.timeEvents) for w in ws])
+
+    if all_open:
+
+        supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+        response = (
+            supabase.table("data")
+            .insert({"positions": [p.model_dump_json() for p in positions]})
+            .execute()
+        )
+        print(response)
+    else:
+        print("Not all open")
+
+    # Schedule the next run based on absolute time
+    next_run += INTERVAL
+    sleep_time = next_run - time.time()
+
+    if sleep_time > 0:
+        time.sleep(sleep_time)
+    else:
+        # If we’re running behind schedule, skip missed intervals
+        next_run = time.time()
---
+++ test.py
@@ -0,0 +1,191 @@
+import os
+import plotly.graph_objects as go
+import requests
+from models import TradableInstrument, Exchange, WorkingSchedule, Position
+from dotenv import load_dotenv
+from plotly.subplots import make_subplots
+import numpy as np
+load_dotenv()
+
+TRADING212_KEY = os.environ["TRADING212_KEY"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
+SUPABASE_URL = os.environ["SUPABASE_URL"]
+headers = {
+    "Authorization": TRADING212_KEY,
+    "Content-Type": "application/json",
+}
+
+url = "https://demo.trading212.com/api/v0/equity/portfolio"
+response = requests.get(url, headers=headers)
+positions = [Position(**i) for i in response.json()]
+
+from supabase import Client, create_client
+from datetime import datetime, timedelta
+
+date_str = '2025-10-28T08:47:00.425164+00:00'
+dt = datetime.fromisoformat(date_str)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+response = supabase.table("data").select("*").order('created_at').execute()
+data = response.data
+
+times = []
+leverage = []
+non_leverage = []
+
+
+for d in data:
+    # print(d)
+    t = d["created_at"]
+    times.append(t)
+    positions = {
+        Position.model_validate_json(p).ticker: Position.model_validate_json(p)
+        for p in d["positions"]
+    }
+    if "VUAGl_EQ" in positions:
+        non_leverage.append(positions["VUAGl_EQ"].currentPrice)
+    else:
+        non_leverage.append(None)
+
+    if "5LUSl_EQ" in positions:
+        leverage.append(positions["5LUSl_EQ"].currentPrice)
+    else:
+        leverage.append(None)
+
+diff_leverage = np.diff(leverage)
+diff_non_leverage = np.diff(non_leverage)
+diff_leverage = np.insert(diff_leverage, 0, 0)
+diff_non_leverage = np.insert(diff_non_leverage, 0, 0)
+
+
+
+rel_diff_non_leverage=diff_non_leverage/np.array(non_leverage)
+rel_diff_leverage=diff_leverage/np.array(leverage)
+
+
+
+NUM_SHARES_NON_LEVERAGE=100
+NUM_SHARES_LEVERAGE=0
+
+FX=0.9999
+FX=1.0
+from enum import Enum
+
+class State(Enum):
+    OUT_OF_LEVERAGE=0
+    IN_LEVERAGE=1
+
+state=State.OUT_OF_LEVERAGE
+
+date_last_trade=None
+
+for date, price_non_leverage, price_leverage, rel_non_leverage, rel_leverage in zip(times, non_leverage, leverage, rel_diff_non_leverage, rel_diff_leverage):
+    # print(NUM_SHARES_NON_LEVERAGE*price_non_leverage+NUM_SHARES_LEVERAGE*price_leverage, NUM_SHARES_NON_LEVERAGE, NUM_SHARES_LEVERAGE)
+    if NUM_SHARES_LEVERAGE>0 and datetime.fromisoformat(date)-datetime.fromisoformat(date_last_trade)>timedelta(minutes=10):
+
+        value_leverage=NUM_SHARES_LEVERAGE*price_leverage
+        NUM_SHARES_NON_LEVERAGE+=value_leverage/price_non_leverage*FX
+        NUM_SHARES_LEVERAGE=0
+        print(f"Going out of leverage {date} -> {NUM_SHARES_NON_LEVERAGE}")
+        date_last_trade = date
+        print(NUM_SHARES_NON_LEVERAGE * price_non_leverage + NUM_SHARES_LEVERAGE * price_leverage,
+              NUM_SHARES_NON_LEVERAGE, NUM_SHARES_LEVERAGE)
+    else:
+        if rel_non_leverage>0.001 and rel_leverage/rel_non_leverage<3:
+            print(f"Going INTO leverage {date}")
+            print(f"  Non-leverage rose {rel_non_leverage}")
+            print(f"  leverage rose     {rel_leverage}")
+
+            # print(rel_leverage/rel_non_leverage, rel_non_leverage, rel_leverage)
+            MONEY=NUM_SHARES_NON_LEVERAGE*price_non_leverage
+            NUM_SHARES_LEVERAGE=MONEY/price_leverage
+            NUM_SHARES_NON_LEVERAGE=0
+            date_last_trade=date
+            print(NUM_SHARES_NON_LEVERAGE * price_non_leverage + NUM_SHARES_LEVERAGE * price_leverage,
+                  NUM_SHARES_NON_LEVERAGE, NUM_SHARES_LEVERAGE)
+print(NUM_SHARES_NON_LEVERAGE)
+            #print(date, price_non_leverage, price_leverage, rel_non_leverage, rel_leverage)
+
+######
+
+dts = [datetime.fromisoformat(t) for t in times]
+day=[datetime.fromisoformat(t).hour*60+datetime.fromisoformat(t).minute for t in times]
+
+go.Figure(
+    go.Scatter(
+        x=non_leverage,
+        y=leverage,
+        text=day,
+        marker=dict(color=day),
+        mode='markers'
+)).show()
+
+buys={'x':[], 'y':[]}
+sells={'x':[], 'y':[]}
+lev_moves=[]
+current_base_value=non_leverage[0]
+lev_at_current_base_value=leverage[0]
+current_base_value_since=dts[0]
+for base, lev, dt in zip(non_leverage, leverage, dts):
+    if base==current_base_value:
+        lev_move=lev-lev_at_current_base_value
+        if lev_move>10 and (dt-current_base_value_since).seconds>300:
+            buys['x'].append(dt)
+            buys['y'].append(base)
+        if lev_move<-10 and (dt-current_base_value_since).seconds>300:
+            sells['x'].append(dt)
+            sells['y'].append(base)
+    else:
+        current_base_value=base
+        current_base_value_since=dt
+        lev_at_current_base_value=lev
+        lev_move=0
+    lev_moves.append(lev_move)
+
+from plotly.subplots import make_subplots
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+trace1=go.Scatter(
+        x=times,
+        y=non_leverage,
+        name='non-leveraged',
+        text=day,
+        marker=dict(color='black', showscale=True,),  # 👈 important: makes the colorbar visible
+        mode='lines')
+trace2=go.Scatter(
+        x=times,
+        y=leverage,
+        name='leveraged',
+        text=day,
+        marker=dict(color='black'),
+        mode='lines')
+trace3=go.Scatter(
+        x=sells['x'],
+        y=sells['y'],
+        name='sells',
+        text=day,
+        marker=dict(color='red'),
+        mode='markers')
+trace4=go.Scatter(
+        x=buys['x'],
+        y=buys['y'],
+        name='buys',
+        text=day,
+        marker=dict(color='green'),
+        mode='markers')
+fig.add_trace(trace1)
+fig.add_trace(trace3)
+fig.add_trace(trace4)
+# fig.add_trace(trace2, secondary_y=True)
+fig.show()
+
+
+99.4, 3087
+99.45,3106
+
+"""
+
+99.4, 3083
+100.0, 3159
+
+(3159-3083)/(100.0-99.4)
+"""
